03513427dfbbde1ba7a416bfe9cc07fad484b09f
Thomas Swayze <teswayze@gmail.com>
2023-03-03 23:47:14 -0600

    Pull specific opening

--- includes/openings.hpp
+++ includes/openings.hpp
@@ -9,3 +9,4 @@
 using BookPosition = std::tuple<bool, Board, std::string>;
 
 std::list<BookPosition> read_openings_from_file(const std::string book_name);
+BookPosition read_specific_opening(const std::string book_name, const std::string opening_name);
--- src/openings.cpp
+++ src/openings.cpp
@@ -3,6 +3,7 @@
 
 # include <fstream>
 # include <sstream>
+# include <stdexcept>
 
 
 BookPosition read_opening_from_line(std::string line){
@@ -25,7 +26,7 @@ BookPosition read_opening_from_line(std::string line){
 
 
 std::list<BookPosition> read_openings_from_file(const std::string book_name){
-	std::string file_path = "openings/" + book_name + ".txt";
+	std::string file_path = "openings/" + book_name + ".book";
 	std::ifstream file_stream(file_path);
 	
 	std::list<BookPosition> pos_list;
@@ -38,18 +39,30 @@ std::list<BookPosition> read_openings_from_file(const std::string book_name){
 }
 
 
+BookPosition read_specific_opening(const std::string book_name, const std::string opening_name){
+	auto all_openings = read_openings_from_file(book_name);
+	
+	for (std::list<BookPosition>::iterator it = openings.begin(); it != openings.end(); ++it){
+	    if (std::get<2>(*it) == opening_name){
+	    	return *it;
+	    }
+	}
+	throw std::invalid_argument(book_name + ", " + opening_name);
+}
+
+
 # include "doctest.h"
 
 TEST_CASE("Read starting position"){
-	auto position = read_opening_from_line("| Starting Position");
+	auto position = read_opening_from_line("| Starting-Position");
 	CHECK(std::get<0>(position));
-	CHECK(std::get<2>(position) == "Starting Position");
+	CHECK(std::get<2>(position) == "Starting-Position");
 	CHECK(std::get<1>(position).White.Pawn == RANK_2);
 	CHECK(std::get<1>(position).Black.Pawn == RANK_7);
 }
 
 TEST_CASE("Read open sicilian"){
-	auto position = read_opening_from_line("e2e4 c7c5 g1f3 b8c6 d2d4 c5d4 f3d4 g8f6 b1c3 | Open Sicilian with 2...Nc6");
+	auto position = read_opening_from_line("e2e4 c7c5 g1f3 b8c6 d2d4 c5d4 f3d4 g8f6 b1c3 | Sicilian-Old");
 	CHECK(not std::get<0>(position));
 	CHECK(std::get<1>(position).White.Knight == (ToMask(C3) | ToMask(D4)));
 	CHECK(std::get<1>(position).Black.Knight == (ToMask(C6) | ToMask(F6)));
@@ -62,5 +75,12 @@ TEST_CASE("Lengths meet expectations"){
 	CHECK(read_openings_from_file("D").size() == 10);
 	CHECK(read_openings_from_file("E").size() == 20);
 	CHECK(read_openings_from_file("F").size() == 50);
+}
 
+TEST_CASE("Read specific opeing from file"){
+	auto opsition = read_specific_opening("F", "Caro-Kann-Advance");
+	BitMask expected_white_pawn = RANK_2 ^ (ToMask(D2) | ToMask(E2) | ToMask(D4) | ToMask(E5));
+	CHECK(std::get<1>(position).White.Pawn == expected_white_pawn);
+	BitMask expected_white_pawn = RANK_7 ^ (ToMask(C7) | ToMask(C7) | ToMask(C6) | ToMask(D5));
+	CHECK(std::get<1>(position).White.Pawn == expected_white_pawn);
 }
