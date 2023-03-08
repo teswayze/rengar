897e97fa6424fc64e74407de32460d980457bcaa
Thomas Swayze <teswayze@gmail.com>
2023-03-08 00:45:12 -0500

    Better unit tests

--- src/board.cpp
+++ src/board.cpp
@@ -34,6 +34,16 @@ template HalfBoard remove_piece<false>(const HalfBoard, const Square);
 
 # include "doctest.h"
 
+
+template <bool white>
+inline void check_consistent(HalfBoard h){
+	auto recomputed = static_eval_info<white>(h.Pawn, h.Knight, h.Bishop, h.Rook, h.Queen, h.King);
+	CHECK(h.EvalInfo.mg == recomputed.mg);
+	CHECK(h.EvalInfo.eg == recomputed.eg);
+	CHECK(h.EvalInfo.phase_count == recomputed.phase_count);
+}
+
+
 TEST_CASE("Removing pieces from white starting position"){
 	HalfBoard h = from_masks<true>(RANK_2, ToMask(B1) | ToMask(G1), ToMask(C1) | ToMask(F1),
 			ToMask(A1) | ToMask(H1), ToMask(D1), ToMask(E1), ToMask(A1) | ToMask(H1));
@@ -41,38 +51,33 @@ TEST_CASE("Removing pieces from white starting position"){
 	HalfBoard no_g2 = remove_piece<true>(h, G2);
 	CHECK(no_g2.Pawn == (RANK_2 & ~ToMask(G2)));
 	CHECK(no_g2.All == (RANK_1 | RANK_2 & ~ToMask(G2)));
-	CHECK(no_g2.EvalInfo.phase_count == 12);
-	CHECK(h.EvalInfo.mg - no_g2.EvalInfo.mg == 120);
-	CHECK(h.EvalInfo.eg - no_g2.EvalInfo.eg == 96);
+	check_consistent<true>(no_g2);
 	
 	HalfBoard no_b1 = remove_piece<true>(h, B1);
 	CHECK(no_b1.Knight == ToMask(G1));
 	CHECK(no_b1.All == (RANK_2 | RANK_1 & ~ToMask(B1)));
-	CHECK(no_b1.EvalInfo.phase_count == 11);
-	CHECK(h.EvalInfo.mg - no_b1.EvalInfo.mg == 316);
-	CHECK(h.EvalInfo.eg - no_b1.EvalInfo.eg == 230);
+	check_consistent<true>(no_b1);
 	
 	HalfBoard no_c1 = remove_piece<true>(h, C1);
 	CHECK(no_c1.Bishop == ToMask(F1));
 	CHECK(no_c1.All == (RANK_2 | RANK_1 & ~ToMask(C1)));
-	CHECK(no_c1.EvalInfo.phase_count == 11);
-	CHECK(h.EvalInfo.mg - no_c1.EvalInfo.mg == 351);
-	CHECK(h.EvalInfo.eg - no_c1.EvalInfo.eg == 274);
+	check_consistent<true>(no_c1);
 		
 	HalfBoard no_a1 = remove_piece<true>(h, A1);
 	CHECK(no_a1.Rook == ToMask(H1));
 	CHECK(no_a1.All == (RANK_2 | RANK_1 & ~ToMask(A1)));
 	CHECK(no_a1.Castle == ToMask(H1));
-	CHECK(no_a1.EvalInfo.phase_count == 10);
-	CHECK(h.EvalInfo.mg - no_a1.EvalInfo.mg == 458);
-	CHECK(h.EvalInfo.eg - no_a1.EvalInfo.eg == 503);
+	check_consistent<true>(no_a1);
 	
 	HalfBoard no_d1 = remove_piece<true>(h, D1);
 	CHECK(no_d1.Queen == EMPTY_BOARD);
 	CHECK(no_d1.All == (RANK_2| RANK_1 & ~ToMask(D1)));
-	CHECK(no_d1.EvalInfo.phase_count == 8);
-	CHECK(h.EvalInfo.mg - no_d1.EvalInfo.mg == 1035);
-	CHECK(h.EvalInfo.eg - no_d1.EvalInfo.eg == 893);
+	check_consistent<true>(no_d1);
+	
+	HalfBoard same = remove_piece<true>(h, B8);
+	CHECK(h == same);
+	CHECK(h.All == same.All);
+	check_consistent<true>(same);
 }
 
 TEST_CASE("Removing pieces from black starting position"){
@@ -82,36 +87,31 @@ TEST_CASE("Removing pieces from black starting position"){
 	HalfBoard no_g7 = remove_piece<false>(h, G7);
 	CHECK(no_g7.Pawn == (RANK_7 & ~ToMask(G7)));
 	CHECK(no_g7.All == (RANK_8 | RANK_7 & ~ToMask(G7)));
-	CHECK(no_g7.EvalInfo.phase_count == 12);
-	CHECK(h.EvalInfo.mg - no_g7.EvalInfo.mg == 120);
-	CHECK(h.EvalInfo.eg - no_g7.EvalInfo.eg == 96);
+	check_consistent<false>(no_g7);
 	
 	HalfBoard no_g8 = remove_piece<false>(h, G8);
 	CHECK(no_g8.Knight == ToMask(B8));
 	CHECK(no_g8.All == (RANK_7 | RANK_8 & ~ToMask(G8)));
-	CHECK(no_g8.EvalInfo.phase_count == 11);
-	CHECK(h.EvalInfo.mg - no_g8.EvalInfo.mg == 318);
-	CHECK(h.EvalInfo.eg - no_g8.EvalInfo.eg == 231);
+	check_consistent<false>(no_g8);
 	
 	HalfBoard no_c8 = remove_piece<false>(h, C8);
 	CHECK(no_c8.Bishop == ToMask(F8));
 	CHECK(no_c8.All == (RANK_7 | RANK_8 & ~ToMask(C8)));
-	CHECK(no_c8.EvalInfo.phase_count == 11);
-	CHECK(h.EvalInfo.mg - no_c8.EvalInfo.mg == 351);
-	CHECK(h.EvalInfo.eg - no_c8.EvalInfo.eg == 274);
+	check_consistent<false>(no_c8);
 		
 	HalfBoard no_h8 = remove_piece<false>(h, H8);
 	CHECK(no_h8.Rook == ToMask(A8));
 	CHECK(no_h8.All == (RANK_7 | RANK_8 & ~ToMask(H8)));
 	CHECK(no_h8.Castle == ToMask(A8));
-	CHECK(no_h8.EvalInfo.phase_count == 10);
-	CHECK(h.EvalInfo.mg - no_h8.EvalInfo.mg == 451);
-	CHECK(h.EvalInfo.eg - no_h8.EvalInfo.eg == 492);
+	check_consistent<false>(no_h8);
 	
 	HalfBoard no_d8 = remove_piece<false>(h, D8);
 	CHECK(no_d8.Queen == EMPTY_BOARD);
 	CHECK(no_d8.All == (RANK_7 | RANK_8 & ~ToMask(D8)));
-	CHECK(no_d8.EvalInfo.phase_count == 8);
-	CHECK(h.EvalInfo.mg - no_d8.EvalInfo.mg == 1035);
-	CHECK(h.EvalInfo.eg - no_d8.EvalInfo.eg == 893);
+	check_consistent<false>(no_d8);
+	
+	HalfBoard same = remove_piece<true>(h, F1);
+	CHECK(h == same);
+	CHECK(h.All == same.All);
+	check_consistent<false>(same);
 }
