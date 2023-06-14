04dc039459a0bf45170875e196952d20363d71b4
Thomas Swayze <teswayze@gmail.com>
2023-06-14 00:28:09 -0500

    Clean up macro a bit

--- includes/register_params.hpp
+++ includes/register_params.hpp
@@ -2,12 +2,9 @@
 
 void register_param(std::string param_name, int *param_ref);
 
-# define REGISTER_MUTABLE_PARAM(name, value)             \
-    int name = value;                                    \
-    namespace                                            \
-    {                                                    \
-    struct _temp_class_##name                            \
-    {                                                    \
+# define REGISTER_MUTABLE_PARAM(name, value)                      \
+    int name = value;                                             \
+    struct _temp_class_##name                                     \
+    {                                                             \
     	_temp_class_##name() { register_param(#name, &name); }    \
-    } _temp_instance_##name;                                                 \
-    }
+    } _temp_instance_##name;                                      \
