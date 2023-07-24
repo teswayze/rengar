a4daa79d85c7f2a816d1a13afcf38abfbe3cee22
Thomas Swayze <teswayze@gmail.com>
2023-07-23 21:17:41 -0400

    Delete old history code and test new

--- includes/history.hpp
+++ includes/history.hpp
@@ -1,26 +1,6 @@
 # pragma once
 
-# include <memory>
 # include <array>
-
-# include "board.hpp"
-
-template <typename T>
-struct ListNode{
-	const T head;
-	const std::shared_ptr<const ListNode<T>> tail;
-
-	ListNode(const T h, const std::shared_ptr<const ListNode<T>> t) :
-		head(h), tail(t) { }
-};
-
-using History = std::shared_ptr<const ListNode<uint64_t>>;
-
-History extend_history(const Board &board, const History history);
-bool exists_in_history(const Board &board, const History history);
-History remove_single_repetitions(const History history);
-History remove_hash_from_history(const History history, const Board &board);
-
 
 struct History2{
 	std::array<uint64_t, 256> hash_array;
--- src/history.cpp
+++ src/history.cpp
@@ -1,41 +1,4 @@
-# include "board.hpp"
 # include "history.hpp"
-
-History extend_history(const Board &board, const History history){
-	return std::make_shared<ListNode<uint64_t>>(board.EvalInfo.hash, history);
-}
-
-template <bool check_second>
-bool exists_in_history_helper(const uint64_t hash, const History history){
-	// Only check every other to allow triangulation e.g.
-	if (history == nullptr) { return false; }
-	if (history->tail == nullptr) { return false; }
-	if (check_second and (hash == history->tail->head)) { return true; }
-	return exists_in_history_helper<true>(hash, history->tail->tail);
-}
-
-bool exists_in_history(const Board &board, const History history){
-	return exists_in_history_helper<false>(board.EvalInfo.hash, history);
-}
-
-History remove_single_repetitions(const History history){
-	if (history == nullptr) return nullptr;
-	History trimmed_tail = remove_single_repetitions(history->tail);
-	if (exists_in_history_helper<false>(history->head, history->tail)) {
-		return std::make_shared<ListNode<uint64_t>>(history->head, trimmed_tail);
-	}
-	if (trimmed_tail == nullptr) return nullptr;
-	return std::make_shared<ListNode<uint64_t>>(0ull, trimmed_tail);
-}
-
-History remove_hash_from_history(const History history, const Board &board){
-	if (history == nullptr) return history;
-	if (history->tail == nullptr) return history;
-	auto second_head = (history->tail->head == board.EvalInfo.hash) ? 0ull : history->tail->head;
-	return std::make_shared<ListNode<uint64_t>>(history->head, std::make_shared<ListNode<uint64_t>>(second_head,
-			remove_hash_from_history(history->tail->tail, board)));
-}
-
 
 bool History2::is_repetition(const uint64_t hash) const{
 	bool twofold = false;
@@ -67,48 +30,65 @@ History2 History2::extend_root(const uint64_t hash){
 
 # ifndef DOCTEST_CONFIG_DISABLE
 # include "doctest.h"
+# include "board.hpp"
 # include "parse_format.hpp"
 
 
 TEST_CASE("Repetition checking"){
 	bool wtm; Board board;
 	wtm = parse_fen("8/1p6/1P5p/3Rr2k/p3r1q1/3Q2P1/5B2/6K1 b - - 3 49", board);
-	History history0 = nullptr;
-	CHECK(not exists_in_history(board, history0));
+	History2 root_history;
+	History2 search_history;
+	CHECK(not root_history.is_repetition(board.EvalInfo.hash));
+	CHECK(not search_history.is_repetition(board.EvalInfo.hash));
 
-	History history1 = extend_history(board, history0);
+	root_history = root_history.extend_root(board.EvalInfo.hash);
+	search_history = search_history.extend(board.EvalInfo.hash);
 	make_move<false>(board, move_from_squares(G4, F5, QUEEN_MOVE));
-	CHECK(not exists_in_history(board, history1));
+	CHECK(not root_history.is_repetition(board.EvalInfo.hash));
+	CHECK(not search_history.is_repetition(board.EvalInfo.hash));
 
-	History history2 = extend_history(board, history1);
+	root_history = root_history.extend_root(board.EvalInfo.hash);
+	search_history = search_history.extend(board.EvalInfo.hash);
 	make_move<true>(board, move_from_squares(D3, D1, QUEEN_MOVE));
-	CHECK(not exists_in_history(board, history2));
+	CHECK(not root_history.is_repetition(board.EvalInfo.hash));
+	CHECK(not search_history.is_repetition(board.EvalInfo.hash));
 
-	History history3 = extend_history(board, history2);
+	root_history = root_history.extend_root(board.EvalInfo.hash);
+	search_history = search_history.extend(board.EvalInfo.hash);
 	make_move<false>(board, move_from_squares(F5, G4, QUEEN_MOVE));
-	CHECK(not exists_in_history(board, history3));
+	CHECK(not root_history.is_repetition(board.EvalInfo.hash));
+	CHECK(not search_history.is_repetition(board.EvalInfo.hash));
 
-	History history4 = extend_history(board, history3);
+	root_history = root_history.extend_root(board.EvalInfo.hash);
+	search_history = search_history.extend(board.EvalInfo.hash);
 	make_move<true>(board, move_from_squares(D1, D3, QUEEN_MOVE));
-	CHECK(exists_in_history(board, history4));
-	CHECK(not exists_in_history(board, remove_single_repetitions(history4)));
+	CHECK(not root_history.is_repetition(board.EvalInfo.hash));
+	CHECK(search_history.is_repetition(board.EvalInfo.hash));
 
-	History history5 = extend_history(board, history4);
+	root_history = root_history.extend_root(board.EvalInfo.hash);
+	search_history = search_history.extend(board.EvalInfo.hash);
 	make_move<false>(board, move_from_squares(E5, F5, ROOK_MOVE));
-	CHECK(not exists_in_history(board, history5));
+	CHECK(not root_history.is_repetition(board.EvalInfo.hash));
+	CHECK(not search_history.is_repetition(board.EvalInfo.hash));
 
-	History history6 = extend_history(board, history5);
+	root_history = root_history.extend_root(board.EvalInfo.hash);
+	search_history = search_history.extend(board.EvalInfo.hash);
 	make_move<true>(board, move_from_squares(G1, F1, KING_MOVE));
-	CHECK(not exists_in_history(board, history6));
+	CHECK(not root_history.is_repetition(board.EvalInfo.hash));
+	CHECK(not search_history.is_repetition(board.EvalInfo.hash));
 
-	History history7 = extend_history(board, history6);
+	root_history = root_history.extend_root(board.EvalInfo.hash);
+	search_history = search_history.extend(board.EvalInfo.hash);
 	make_move<false>(board, move_from_squares(F5, E5, ROOK_MOVE));
-	CHECK(not exists_in_history(board, history7));
+	CHECK(not root_history.is_repetition(board.EvalInfo.hash));
+	CHECK(not search_history.is_repetition(board.EvalInfo.hash));
 
-	History history8 = extend_history(board, history7);
+	root_history = root_history.extend_root(board.EvalInfo.hash);
+	search_history = search_history.extend(board.EvalInfo.hash);
 	make_move<true>(board, move_from_squares(F1, G1, KING_MOVE));
-	CHECK(exists_in_history(board, history8));
-	CHECK(exists_in_history(board, remove_single_repetitions(history8)));
+	CHECK(root_history.is_repetition(board.EvalInfo.hash));
+	CHECK(search_history.is_repetition(board.EvalInfo.hash));
 }
 
 # endif
