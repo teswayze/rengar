f520b1cd11ce20180234ddf59813999889936e78
Thomas Swayze <teswayze@gmail.com>
2023-08-30 00:14:14 -0500

    WIP - separate tables for OSC and SSC middlegame

--- includes/weights/ssc_middlegame.hpp
+++ includes/weights/ssc_middlegame.hpp
@@ -2,13 +2,15 @@
 
 # include <array>
 
-const int mg_pawn = 63;
-const int mg_knight = 311;
-const int mg_bishop = 297;
-const int mg_rook = 372;
-const int mg_queen = 814;
+const int ssc_mg_pawn = 63;
+const int ssc_mg_knight = 311;
+const int ssc_mg_bishop = 297;
+const int ssc_mg_rook = 372;
+const int ssc_mg_queen = 814;
 
-const std::array<int, 64> mg_pawn_table = {
+// All tables can be viewed from white's perspective with both kings on the kingside
+
+const std::array<int, 64> ssc_mg_pawn_table = {
 	  0,   0,   0,   0,   0,   0,   0,   0,
 	 90, 128,  65,  91,  71, 120,  26, -14,
 	 -6,   5,  28,  28,  60,  55,  23, -21,
@@ -18,7 +20,7 @@ const std::array<int, 64> mg_pawn_table = {
 	-27,  -8, -23, -30, -17,  26,  37, -30,
 	  0,   0,   0,   0,   0,   0,   0,   0,
 };
-const std::array<int, 64> mg_knight_table = {
+const std::array<int, 64> ssc_mg_knight_table = {
 	-179,  -98,  -49,  -55,   48, -111,  -26, -120,
 	 -80,  -45,   55,   29,   10,   45,   -3,  -27,
 	 -59,   49,   22,   57,   74,  120,   62,   32,
@@ -28,7 +30,7 @@ const std::array<int, 64> mg_knight_table = {
 	 -44,  -60,  -23,    0,  -17,    1,  -27,  -27,
 	-119,  -30,  -63,  -49,  -40,  -39,  -33,  -36,
 };
-const std::array<int, 64> mg_bishop_table = {
+const std::array<int, 64> ssc_mg_bishop_table = {
 	-37, -14, -85, -48, -21, -52,  -6, -18,
 	-22,   2, -14,  -7,  17,  37,   4, -43,
 	 -4,  19,  20,  25,  16,  43,  26,  -8,
@@ -38,7 +40,7 @@ const std::array<int, 64> mg_bishop_table = {
 	 -5,   3,   2,  -6,   1,   4,  30,   0,
 	-34, -13, -16, -31,  -7, -12, -38, -15,
 };
-const std::array<int, 64> mg_rook_table = {
+const std::array<int, 64> ssc_mg_rook_table = {
 	 18,  47,  30,  45,  52,  27,  34,  48,
 	 24,  38,  59,  69,  71,  72,  31,  47,
 	 -7,  22,  26,  40,  21,  48,  57,  14,
@@ -48,7 +50,7 @@ const std::array<int, 64> mg_rook_table = {
 	-44, -14, -20,  -7, -15,   5,  -7, -71,
 	 -7, -10,  -1,   3,   5,  -4, -36, -17,
 };
-const std::array<int, 64> mg_queen_table = {
+const std::array<int, 64> ssc_mg_queen_table = {
 	-30, -19,  22,   1,  39,  43,  38,  43,
 	-23, -47, -16,   1, -12,  57,  25,  46,
 	-25, -22,  -3,   5,  24,  63,  48,  54,
@@ -58,13 +60,13 @@ const std::array<int, 64> mg_queen_table = {
 	-40, -15,   6,   2,   9,  18,  -6,  -8,
 	 -3, -18,  -2,   3, -19, -30, -40, -56,
 };
-const std::array<int, 64> mg_king_table = {
-	-76,  17,   8, -23, -64, -33,  -7,  -3,
-	 19, -14, -14, -21, -23, -24, -46, -36,
-	-19,  15, -15, -25, -20, -11,  -9, -23,
-	-21, -31, -16, -30, -25, -37, -29, -51,
-	-62,  -1, -44, -52, -54, -54, -46, -54,
-	-25, -23, -35, -62, -61, -46, -28, -41,
-	-18,  -6, -24, -82, -65, -43,   4,   5,
-	-33,  20,  -7, -80, -25, -45,  25,  12,
+const std::array<int, 64> ssc_mg_king_table = {
+	  0,   0,   0,   0, -64, -33,  -7,  -3,
+	  0,   0,   0,   0, -23, -24, -46, -36,
+	  0,   0,   0,   0, -20, -11,  -9, -23,
+	  0,   0,   0,   0, -25, -37, -29, -51,
+	  0,   0,   0,   0, -54, -54, -46, -54,
+	  0,   0,   0,   0, -61, -46, -28, -41,
+	  0,   0,   0,   0, -65, -43,   4,   5,
+	  0,   0,   0,   0, -25, -45,  25,  12,
 };
