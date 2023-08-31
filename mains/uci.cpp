fe25ecd313165ddec5ef08d33b339784e335a64b
Thomas Swayze <teswayze@gmail.com>
2023-08-30 23:50:05 -0500

    Fix eval info in main

--- mains/uci.cpp
+++ mains/uci.cpp
@@ -188,7 +188,10 @@ int main() {
 			search_stats();
 		}
 		if (command == "eval"){
-			std::cout << "mg = " << board.EvalInfo.mg << std::endl;
+			const auto mg_pst_eval = (board.White.King % 8 >= 4) ? 
+				((board.Black.King % 8 >= 4) ? board.EvalInfo.mg_kk : board.EvalInfo.mg_kq) :
+				((board.Black.King % 8 >= 4) ? board.EvalInfo.mg_qk : board.EvalInfo.mg_qq);
+			std::cout << "mg = " << mg_pst_eval << std::endl;
 			std::cout << "eg = " << board.EvalInfo.eg << std::endl;
 			std::cout << "phase_count = " << board.EvalInfo.phase_count << std::endl;
 			std::cout << "eval = " << (wtm ? eval<true>(board) : -eval<false>(board)) << std::endl;
