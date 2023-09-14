de7a0b210c2de788011a53af59ef6abd821d9433
Thomas Swayze <teswayze@gmail.com>
2023-09-13 22:22:18 -0500

    Read game results

--- includes/read_game.hpp
+++ includes/read_game.hpp
@@ -9,3 +9,4 @@ using int_vec = std::vector<int>;
 move_vec read_game(std::string game_path);
 string_vec find_game_paths(int num_tournaments, char **tournament_names);
 std::vector<move_vec> load_games(string_vec game_paths);
+int_vec read_results(string_vec game_paths);
--- src/read_game.cpp
+++ src/read_game.cpp
@@ -6,6 +6,7 @@
 
 const std::string games_dir = "games/";
 const std::string pgn_file_name = "/game.pgn";
+const std::string results_file_name = "/results.txt";
 
 move_vec read_game(std::string game_path){
 	std::ifstream game_file;
@@ -32,7 +33,7 @@ string_vec find_game_paths(int num_tournaments, char **tournament_names){
 	for (int i = 0; i < num_tournaments; i++){
 		for (const auto & entry : std::filesystem::directory_iterator(games_dir + tournament_names[i])){
 			std::string path = entry.path();
-			game_paths.push_back(path + pgn_file_name);
+			game_paths.push_back(path);
 		}
 	}
 	return game_paths;
@@ -42,8 +43,27 @@ std::vector<move_vec> load_games(string_vec game_paths){
 	std::cout << "Found " << game_paths.size() << " games to load..." << std::endl;
 	std::vector<move_vec> games;
 	for (const auto path : game_paths){
-		games.push_back(read_game(path));
+		games.push_back(read_game(path + pgn_file_name));
 		if (games.size() % 1000 == 0) std::cout << "Loaded " << games.size() << " games..." << std::endl;
 	}
 	return games;
+}
+
+int_vec read_results(string_vec game_paths){
+	std::cout << "Reading results from " << game_paths.size() << " games..." << std::endl;
+	int_vec results;
+	for (const auto path : game_paths){
+		std::ifstream game_file;
+		game_file.open(path + results_file_name);
+		std::string res_str;
+		std::getline(game_file, res_str, ' ');
+
+		if (res_str == "White") results.push_back(1);
+		else if (res_str == "Black") results.push_back(-1);
+		else if (res_str == "Draw") results.push_back(0);
+		else throw std::invalid_argument("Expected 'White', 'Black' or 'Draw' but saw " + res_str);
+
+		if (results.size() % 1000 == 0) std::cout << "Read " << results.size() << " results..." << std::endl;
+	}
+	return results;
 }
