fec6dc1f00f7623f5337734c0d6603a40b95cc43
Thomas Swayze <teswayze@gmail.com>
2024-03-27 21:33:26 -0500

    Timer in main

--- src/datagen/main.cpp
+++ src/datagen/main.cpp
@@ -2,6 +2,7 @@
 # include <cstdlib>
 
 # include "opening_tree.hpp"
+# include "../timer.hpp"
 
 void send_error_message(){
     std::cout << "Usage: ./datagen <num_games> <search_depth>" << std::endl;
@@ -13,8 +14,12 @@ int main(int argc, char **argv){
     int num_games = std::atoi(argv[1]);
     int search_depth = std::atoi(argv[2]);
 
+    Timer t;
+    t.start();
     OpeningTree tree = init_opening_tree();
     for (int i = 1; i < num_games; i++) tree.deepen(search_depth);
-    std::cout << "Successuflly built tree!" << std::endl;
+    t.stop();
+
+    std::cout << "Successuflly built tree in " << t.ms_elapsed() / 1000 << " seconds" << std::endl;
     tree.show();
 }
