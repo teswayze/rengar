ab7da49566fc7458a11a84b23b0834377bff034d
Thomas Swayze <teswayze@gmail.com>
2024-05-05 17:42:12 +0100

    Combine headers

--- src/updatable.hpp
+++ src/updatable.hpp
@@ -1,7 +1,26 @@
 # pragma once
 
-# include "layer_types.hpp"
+# include <immintrin.h>
 # include "hashing.hpp"
+
+struct FirstLayer{
+    // Invariant under rotation and reflection
+    // Examples: game phase, position openness, position sharpness
+    __m256i full_symm;
+
+    // Invariant under horizontal reflection; negates under vertical reflection
+    // Examples: material difference, piece activity, pawn structure quality
+    // The final evaluation will look like this
+    __m256i vert_asym;
+
+    // Invariant under vertical reflection; negates under horizontal reflection
+    // Examples: castling side if SSC, pawn storm/shield if OSC, relative color complex strength
+    __m256i horz_asym;
+
+    // Invariant under 180 degree rotation; negates on either reflection
+    // Examples: castling side if OSC, pawn chain direction, pawn race orientation
+    __m256i rotl_asym;
+};
 
 extern std::array<FirstLayer, 184> first_layer_weights;
 
