aa218f6a22bb7686d0c3a0607dc79326e90482da
Thomas Swayze <teswayze@gmail.com>
2024-05-05 14:29:58 +0100

    Some random first layer weights

--- src/updatable.hpp
+++ src/updatable.hpp
@@ -1,5 +1,24 @@
-# include "first_layer.hpp"
+# include "weights/init_random.hpp"
 # include "hashing.hpp"
+
+// 24 options for pawns, 32 options for each piece
+// If you tried to access a pawn on your own backrank, that would look like a knight on the enemy backrank
+// If you tried to access a pawn on the enemy backrank, that would access a negative index
+const std::array<int, 6> piece_idx_offsets = {-4, 24, 56, 88, 120, 152};
+
+template <bool white, bool add, Piece piece>
+inline void update_first_layer(FirstLayer &layer, const Square square){
+    const bool flip_h = square & 4;
+    const int idx = piece_idx_offsets[piece]
+        + 4 * (white ? (7 - square / 8) : (square / 8)) // Rank of square (0 is enemy backrank, 7 is own backrank)
+        + (flip_h ? (7 - square % 8) : (square % 8)); // File of square (0 if A/H, 3 if D/E);
+    const FirstLayer to_update = first_layer_weights[idx];
+
+    layer.full_symm = (add ? _mm256_add_epi16 : _mm256_sub_epi16)(layer.full_symm, to_update.full_symm);
+    layer.vert_asym = ((add ^ white) ? _mm256_add_epi16 : _mm256_sub_epi16)(layer.vert_asym, to_update.vert_asym);
+    layer.horz_asym = ((add ^ flip_h) ? _mm256_add_epi16 : _mm256_sub_epi16)(layer.horz_asym, to_update.horz_asym);
+    layer.rotl_asym = ((add ^ white ^ flip_h) ? _mm256_add_epi16 : _mm256_sub_epi16)(layer.rotl_asym, to_update.rotl_asym);
+}
 
 struct EfficientlyUpdatable{
     FirstLayer l1;
