78cc664349a26709c93dc4695edc49b13fceb35f
Thomas Swayze <teswayze@gmail.com>
2024-05-05 08:32:21 +0200

    Draw score if 50-move rule is reached

--- src/search.cpp
+++ src/search.cpp
@@ -77,6 +77,7 @@ template <bool white, bool allow_pruning=true>
 std::tuple<int, VariationView, int> search_helper(const Board &board, const int depth, const int alpha, const int beta,
 		History &history, const VariationView last_pv, const Move sibling_killer1, const Move sibling_killer2){
 	if (is_insufficient_material(board)){ return std::make_tuple(0, last_pv.nullify(), history.curr_idx); }
+	if (history.curr_idx - history.irreversible_idx >= 100) return std::make_tuple(0, last_pv.nullify(), history.curr_idx);
 	int index_of_repetition = history.index_of_repetition(board.EvalInfo.hash);
 	if (index_of_repetition != -1){ 
 		repetitions++;
