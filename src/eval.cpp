653eb8a43dbb30f09f9d305707e5ee52bd491826
Thomas Swayze <teswayze@gmail.com>
2024-05-12 11:05:55 -0500

    Refactor first layer weights to just be arrays

--- src/eval.cpp
+++ src/eval.cpp
@@ -1,8 +1,8 @@
 # include "eval.hpp"
 
-extern Vector w_l1_tempo;
+extern Vector w_l1_tempo_va;
 
-extern Vector w_l2_fs_bias;
+extern Vector w_l2_bias_fs;
 extern Matrix w_l2_fs_fs;
 extern Matrix w_l2_absva_fs;
 extern Matrix w_l2_absha_fs;
@@ -17,14 +17,13 @@ extern Vector w_final_fsxva;
 
 
 inline SecondLayer l1_l2_transition(const FirstLayer l1){
-	auto l2_fs = w_l2_fs_bias;
+	auto l2_fs = w_l2_bias_fs;
 	l2_fs = vector_add(l2_fs, matmul(w_l2_fs_fs, l1.full_symm));
 	l2_fs = vector_add(l2_fs, matmul(w_l2_absva_fs, vector_abs(l1.vert_asym)));
 	l2_fs = vector_add(l2_fs, matmul(w_l2_absha_fs, vector_abs(l1.horz_asym)));
 	l2_fs = vector_add(l2_fs, matmul(w_l2_absra_fs, vector_abs(l1.rotl_asym)));
 
-	auto l2_va = vector_zero;
-	l2_va = vector_add(l2_va, matmul(w_l2_va_va, l1.vert_asym));
+	auto l2_va = matmul(w_l2_va_va, l1.vert_asym);
 	l2_va = vector_add(l2_va, matmul(w_l2_fsxva_va, vector_clamp_mul(l1.full_symm, l1.vert_asym)));
 	l2_va = vector_add(l2_va, matmul(w_l2_haxra_va, vector_clamp_mul(l1.horz_asym, l1.rotl_asym)));
 
@@ -40,7 +39,7 @@ inline int eval_from_l2(const SecondLayer l2){
 template <bool wtm>
 ForwardPassOutput forward_pass(const Board &board){
 	auto l1 = board.ue.l1;
-	l1.vert_asym = (wtm ? vector_add : vector_sub)(l1.vert_asym, w_l1_tempo);
+	l1.vert_asym = (wtm ? vector_add : vector_sub)(l1.vert_asym, w_l1_tempo_va);
 	const auto l2 = l1_l2_transition(l1);
 	const int eval_ = eval_from_l2(l2);
 	return ForwardPassOutput{l1, l2, eval_};
--- src/updatable.hpp
+++ src/updatable.hpp
@@ -22,7 +22,10 @@ struct FirstLayer{
     Vector rotl_asym;
 };
 
-extern std::array<FirstLayer, 184> first_layer_weights;
+extern std::array<Vector, 184> w_l1_pst_fs;
+extern std::array<Vector, 184> w_l1_pst_va;
+extern std::array<Vector, 184> w_l1_pst_ha;
+extern std::array<Vector, 184> w_l1_pst_ra;
 
 // 24 options for pawns, 32 options for each piece
 // If you tried to access a pawn on your own backrank, that would look like a knight on the enemy backrank
@@ -35,12 +38,11 @@ inline void update_first_layer(FirstLayer &layer, const Square square){
     const int idx = piece_idx_offsets[piece]
         + 4 * (white ? (7 - square / 8) : (square / 8)) // Rank of square (0 is enemy backrank, 7 is own backrank)
         + (flip_h ? (7 - square % 8) : (square % 8)); // File of square (0 if A/H, 3 if D/E);
-    const FirstLayer to_update = first_layer_weights[idx];
 
-    layer.full_symm = (add ? vector_add : vector_sub)(layer.full_symm, to_update.full_symm);
-    layer.vert_asym = ((add ^ white) ? vector_add : vector_sub)(layer.vert_asym, to_update.vert_asym);
-    layer.horz_asym = ((add ^ flip_h) ? vector_add : vector_sub)(layer.horz_asym, to_update.horz_asym);
-    layer.rotl_asym = ((add ^ white ^ flip_h) ? vector_add : vector_sub)(layer.rotl_asym, to_update.rotl_asym);
+    layer.full_symm = (add ? vector_add : vector_sub)(layer.full_symm, w_l1_pst_fs[idx]);
+    layer.vert_asym = ((add ^ white) ? vector_add : vector_sub)(layer.vert_asym, w_l1_pst_va[idx]);
+    layer.horz_asym = ((add ^ flip_h) ? vector_add : vector_sub)(layer.horz_asym, w_l1_pst_ha[idx]);
+    layer.rotl_asym = ((add ^ white ^ flip_h) ? vector_add : vector_sub)(layer.rotl_asym, w_l1_pst_ra[idx]);
 }
 
 struct EfficientlyUpdatable{
