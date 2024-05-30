826822a95c59b0c095cf37575c796c34b689bb11
Thomas Swayze <teswayze@gmail.com>
2024-05-29 23:51:55 -0500

    More tests

--- src/unittest/sgd_test.cpp
+++ src/unittest/sgd_test.cpp
@@ -6,9 +6,8 @@ const Vector a = vector_set(216, 984, -22, -270, 504, 479, 865, 87, -882, 1002, 
 const Vector b = vector_set(-510, 897, -707, -814, 265, -295, 654, -396, -592, 1003, -390, -567, -481, 72, 388, 455);
 
 
-TEST_CASE("SGD weight adjustment"){
+TEST_CASE("SGD weight adjustment by zero"){
     Vector b_copy = b;
-
     auto adjuster = init_sgd_adjuster(&b_copy);
 
     adjuster.update(vector_zero, 1 << 21);
@@ -16,6 +15,11 @@ TEST_CASE("SGD weight adjustment"){
 
     adjuster.update(a, 0);
     check_equal(b, b_copy);
+}
+
+TEST_CASE("SGD weight adjustment"){
+    Vector b_copy = b;
+    auto adjuster = init_sgd_adjuster(&b_copy);
 
     adjuster.update(a, 1 << 21);
     int16_t *a_ptr = (int16_t*) &a;
@@ -24,4 +28,16 @@ TEST_CASE("SGD weight adjustment"){
     for (int i = 0; i < 16; i++){
         CHECK(b_copy_ptr[i] == std::clamp(a_ptr[i] + b_ptr[i], -1023, 1023));
     }
+}
+
+TEST_CASE("SGD weight adjustment in two steps"){
+    Vector b_copy = b;
+    auto adjuster = init_sgd_adjuster(&b_copy);
+    const Vector all_ones = _mm256_set1_epi16(1);
+
+    adjuster.update(all_ones, 3 << 18);
+    check_equal(b, b_copy);
+
+    adjuster.update(all_ones, 3 << 18);
+    check_equal(vector_add(b, all_ones), b_copy);
 }
