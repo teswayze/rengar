1652c06df698856388b5eaaca2207c0fc78f7c5d
Thomas Swayze <teswayze@gmail.com>
2024-08-27 23:11:34 -0500

    Fix warning

--- src/unittest/vector_helpers.hpp
+++ src/unittest/vector_helpers.hpp
@@ -5,10 +5,10 @@
 
 template <size_t n>
 inline void check_equal(const Eigen::Vector<float, n> left, const Eigen::Vector<float, n> right){
-   for (auto i = 0; i < n; i++) CHECK(std::abs(left[i] - right[i]) < 1e-5);
+   for (size_t i = 0; i < n; i++) CHECK(std::abs(left[i] - right[i]) < 1e-5);
 }
 
 template <size_t n>
 inline void check_opposite(const Eigen::Vector<float, n> left, const Eigen::Vector<float, n> right){
-   for (auto i = 0; i < n; i++) CHECK(std::abs(left[i] + right[i]) < 1e-5);
+   for (size_t i = 0; i < n; i++) CHECK(std::abs(left[i] + right[i]) < 1e-5);
 }
