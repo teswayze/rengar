7e02bfa326cb3098c710d867782d35384ed8f0ca
Thomas Swayze <teswayze@gmail.com>
2024-09-18 22:54:25 -0500

    Warm start to 10 steps

--- analysis/spsa.py
+++ analysis/spsa.py
@@ -15,20 +15,22 @@ class SpsaOptimizer:
         self, 
         initial_params: dict[str, float],
         initial_step: dict[str, float],
-        alpha: float,
-        gamma: float,
+        alpha: float = 0.602,
+        gamma: float = 0.101,
+        starting_n: int = 10,
     ):
         self.param_names = list(initial_params.keys())
         self.param_values = np.array([initial_params[k] for k in self.param_names])
-        self.base_step = np.array([initial_step[k] for k in self.param_names])
+
         self.alpha = alpha
         self.gamma = gamma
-        self.trial_no = 1
+        self.trial_no = starting_n
+        self.base_step = np.array([initial_step[k] for k in self.param_names]) * starting_n ** gamma
 
     def generate_trial(self) -> SpsaTrial:
         coin_flips = np.random.randint(0, 2, len(self.param_names)) * 2 - 1
         cn_deltan = self.trial_no ** -self.gamma * self.base_step * coin_flips
-        an = self.trial_no ** -self.alpha * self.base_step
+        an = self.trial_no ** -self.alpha * self.base_step ** 2
         trial = SpsaTrial(
             params_pos=dict(zip(self.param_names, self.param_values + cn_deltan)),
             params_neg=dict(zip(self.param_names, self.param_values - cn_deltan)),
